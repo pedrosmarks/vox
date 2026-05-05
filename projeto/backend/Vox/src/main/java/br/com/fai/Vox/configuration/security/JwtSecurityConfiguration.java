048a8c86ea0af6d85393bc058d9a9e620dd0296f
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2026-05-05 01:05:17 -0300

    feat: implementado as configuracoes do login com jwt

---
+++ projeto/backend/Vox/src/main/java/br/com/fai/Vox/configuration/security/JwtSecurityConfiguration.java
@@ -0,0 +1,54 @@
+package br.com.fai.Vox.configuration.security;
+
+import br.com.fai.Vox.domain.UserModel;
+import br.com.fai.Vox.implementation.service.authentication.jwt.JwtRequestFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Profile("jwt")
+@Configuration
+public class JwtSecurityConfiguration extends BasicSecurityConfiguration{
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    public JwtSecurityConfiguration(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .cors(
+                        cors -> cors.configurationSource(corsConfigurationSource())
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers(
+                                                "/swagger-ui/**",
+                                                "/swagger-ui.html",
+                                                "/v3/api-docs/**",
+                                                "/authenticate"
+                                        ).permitAll()
+                                        .requestMatchers("/api/user/**").hasAnyAuthority(UserModel.UserRole.CITIZEN.name(),
+                                                UserModel.UserRole.ADMINISTRATOR.name())
+                                        .anyRequest().authenticated()
+                )
+                .sessionManagement(
+                        session ->
+                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(
+                        headers ->
+                                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                ).addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
