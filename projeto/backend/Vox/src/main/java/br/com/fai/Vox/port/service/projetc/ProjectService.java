c004b50bc426f1adf961b832b501f3df566322d8
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2026-05-05 16:20:39 -0300

    feat: criado o service de projeto

---
+++ projeto/backend/Vox/src/main/java/br/com/fai/Vox/port/service/projetc/ProjectService.java
@@ -0,0 +1,7 @@
+package br.com.fai.Vox.port.service.projetc;
+
+import br.com.fai.Vox.domain.Project;
+import br.com.fai.Vox.port.service.crud.CrudService;
+
+public interface ProjectService extends CrudService<Project> {
+}
