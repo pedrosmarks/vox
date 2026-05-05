1310168de1b3b32cc812cade026259275934a76a
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2026-05-05 01:03:47 -0300

    feat: criado os dto basicos

---
+++ projeto/backend/Vox/src/main/java/br/com/fai/Vox/domain/dto/AuthenticationDto.java
@@ -0,0 +1,22 @@
+package br.com.fai.Vox.domain.dto;
+
+public class AuthenticationDto {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ projeto/backend/Vox/src/main/java/br/com/fai/Vox/domain/dto/UpdatePasswordDto.java
@@ -0,0 +1,35 @@
+package br.com.fai.Vox.domain.dto;
+
+public class UpdatePasswordDto {
+
+    private int id;
+    private String oldPassword;
+    private String newPassword;
+
+    public UpdatePasswordDto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
