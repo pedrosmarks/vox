f9cd1b0d315738a39247645f7fe95681a22ddfed
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2026-05-05 16:20:32 -0300

    feat: criado o dao de projeto

---
+++ projeto/backend/Vox/src/main/java/br/com/fai/Vox/port/dao/project/ProjectDao.java
@@ -0,0 +1,7 @@
+package br.com.fai.Vox.port.dao.project;
+
+import br.com.fai.Vox.domain.Project;
+import br.com.fai.Vox.port.dao.crud.CrudDao;
+
+public interface ProjectDao extends CrudDao<Project> {
+}
