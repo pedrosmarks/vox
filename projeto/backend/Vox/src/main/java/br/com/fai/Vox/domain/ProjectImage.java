c18a3f1d85c0a21fd20e4b2c1e9fa72988a791dd
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2026-05-05 23:33:47 -0300

    feat: criado o modelo do projeto imagem

---
+++ projeto/backend/Vox/src/main/java/br/com/fai/Vox/domain/ProjectImage.java
@@ -0,0 +1,65 @@
+package br.com.fai.Vox.domain;
+
+import java.time.LocalDateTime;
+
+public class ProjectImage {
+
+    private int id;
+    private int projectId;
+    private String url;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // Construtor padrão
+    public ProjectImage() {
+    }
+
+    public ProjectImage(Integer id, Integer projectId, String url, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.projectId = projectId;
+        this.url = url;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // Getters e Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
