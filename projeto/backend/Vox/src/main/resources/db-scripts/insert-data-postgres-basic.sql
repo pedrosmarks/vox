3dc6cb59276806d48fe6a64b9fa522c18e928561
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2026-05-16 12:10:14 -0300

    refactor: ajustado os enum para que todos fiquem com letra maiuscula

--- projeto/backend/Vox/src/main/resources/db-scripts/insert-data-postgres-basic.sql
+++ projeto/backend/Vox/src/main/resources/db-scripts/insert-data-postgres-basic.sql
@@ -7,12 +7,12 @@ INSERT INTO municipality (name, state) VALUES
 
 INSERT INTO user_model (name, email, cpf, phone, password, role, birth_date, municipality_id)
 VALUES
-('João Ribeiro', 'joao@example.com', '00000000000', '(11) 99999-0000', 'aa', 'citizen', '1990-05-15', 1);
+('João Ribeiro', 'joao@example.com', '00000000000', '(11) 99999-0000', 'aa', 'CITIZEN', '1990-05-15', 1);
 
 INSERT INTO user_model (name, email, cpf, phone, password, role, birth_date, municipality_id)
 VALUES
-('Maria Antonieta', 'maria@example.com', '11111111111', '(11) 98888-1111', 'aa', 'moderator', '1985-10-22', 2);
+('Maria Antonieta', 'maria@example.com', '11111111111', '(11) 98888-1111', 'aa', 'MODERATOR', '1985-10-22', 2);
 
 INSERT INTO user_model (name, email, cpf, phone, password, role, birth_date, municipality_id)
 VALUES
-('Carlos Costa', 'carlos@example.com', '33333333333', '(21) 97777-2222', 'aa', 'admin', '1978-03-08', 3);
+('Carlos Costa', 'carlos@example.com', '33333333333', '(21) 97777-2222', 'aa', 'ADMINISTRATOR', '1978-03-08', 3);
