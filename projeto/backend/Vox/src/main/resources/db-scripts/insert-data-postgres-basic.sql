8f64165cca9fd28208ccb8d80314a526c409e236
Gabriel Silva Alves <gabriel21silvaalves@gmail.com>
2026-09-01 20:21:14 -0300

    feat: adicao de municipios na insercao de dados no banco

--- projeto/backend/Vox/src/main/resources/db-scripts/insert-data-postgres-basic.sql
+++ projeto/backend/Vox/src/main/resources/db-scripts/insert-data-postgres-basic.sql
@@ -1,9 +1,75 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 INSERT INTO municipality (name, state) VALUES
-('São Paulo', 'SP'),
-('Campinas', 'SP'),
-('Rio de Janeiro', 'RJ');
+    -- Minas Gerais
+    ('Santa Rita do Sapucaí', 'MG'),
+    ('Pouso Alegre', 'MG'),
+    ('São Sebastião da Bela Vista', 'MG'),
+    ('Cachoeira de Minas', 'MG'),
+    ('Careaçu', 'MG'),
+    ('São Gonçalo do Sapucaí', 'MG'),
+    ('Itajubá', 'MG'),
+    ('Três Corações', 'MG'),
+
+    -- São Paulo
+    ('São Paulo', 'SP'),
+    ('Campinas', 'SP'),
+    ('Bragança Paulista', 'SP'),
+    ('Atibaia', 'SP'),
+    ('Mogi das Cruzes', 'SP'),
+    ('São José dos Campos', 'SP'),
+    ('Guarulhos', 'SP'),
+    ('Taubaté', 'SP'),
+
+    -- Rio de Janeiro
+    ('Rio de Janeiro', 'RJ'),
+    ('Volta Redonda', 'RJ'),
+    ('Resende', 'RJ'),
+    ('Barra Mansa', 'RJ'),
+    ('Petrópolis', 'RJ'),
+    ('Nova Friburgo', 'RJ'),
+    ('Angra dos Reis', 'RJ'),
+
+    -- Espírito Santo
+    ('Vitória', 'ES'),
+    ('Vila Velha', 'ES'),
+    ('Serra', 'ES'),
+    ('Cariacica', 'ES'),
+    ('Linhares', 'ES'),
+    ('Cachoeiro de Itapemirim', 'ES'),
+
+    -- Paraná
+    ('Curitiba', 'PR'),
+    ('Londrina', 'PR'),
+    ('Maringá', 'PR'),
+    ('Ponta Grossa', 'PR'),
+    ('Cascavel', 'PR'),
+    ('São José dos Pinhais', 'PR'),
+
+    -- Bahia
+    ('Salvador', 'BA'),
+    ('Feira de Santana', 'BA'),
+    ('Vitória da Conquista', 'BA'),
+    ('Juazeiro', 'BA'),
+    ('Ilhéus', 'BA'),
+    ('Itabuna', 'BA'),
+
+    -- Goiás
+    ('Goiânia', 'GO'),
+    ('Aparecida de Goiânia', 'GO'),
+    ('Anápolis', 'GO'),
+    ('Rio Verde', 'GO'),
+    ('Luziânia', 'GO'),
+    ('Catalão', 'GO'),
+
+    -- Santa Catarina
+    ('Florianópolis', 'SC'),
+    ('Joinville', 'SC'),
+    ('Blumenau', 'SC'),
+    ('Chapecó', 'SC'),
+    ('Itajaí', 'SC'),
+    ('São José', 'SC');
+
 
 INSERT INTO user_model (name, email, cpf, phone, password, role, birth_date, municipality_id)
 VALUES
